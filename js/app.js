e8de95f1f43555284b4b21b0809b38d3cfc82d8e
tuanpham <tuanppham@gcs-vn.com>
2019-09-01 18:06:05 +0700

    Implement registration.html

--- js/app.js
+++ js/app.js
@@ -1,20 +1,36 @@
 import Router from './router.js';
+import Helper from "./utility/component-helper.js";
 
 var appvm = new Vue({
-  router: Router.router,
-  el: "#app",
-  data() {
-    return {
-      title: "Phạm Tuân's Blog",
-    };
-  },
-  components: {  },
-  created() {
-    console.log("Vue-App initialized: " + this.$data.title);
-  }
+    router: Router.router,
+    el: "#app",
+    data() {
+        return {
+            isLoggedIn: false,
+            userInfo: "",
+        };
+    },
+    components: {
+        "signing": Helper.loadComponent('assignment.js'),
+    },
+    created() {
+        console.log("Vue-App initialized: " + this.$data.title);
+    },
+    methods: {
+        storeUserInfo: function(userInfo) {
+            this.isLoggedIn = true;
+            this.userInfo = userInfo;
+            console.log("User information has been stored.", userInfo);
+        },
+        logout: function() {
+            this.isLoggedIn = false;
+            this.userInfo = {};
+            console.log("User information has been logged out.");
+        }
+    },
 });
 
 window.Application = {
-  VueApp: appvm,
-  Router: Router
+    VueApp: appvm,
+    Router: Router
 };
