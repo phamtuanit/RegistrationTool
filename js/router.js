4de92a33778054e5665cf24cde7723f3ceaa677b
tuanpham <tuanppham@gcs-vn.com>
2019-09-07 17:27:23 +0700

    Fixed router

--- js/router.js
+++ js/router.js
@@ -1,9 +1,7 @@
 import Helper from "./utility/component-helper.js";
 export default {
     router: new VueRouter({
-        mode: 'history',
         routes: [
-            { path: '/survey', component: Helper.loadComponent('survey.js') },
             { path: '/registration', component: Helper.loadComponent('registration.js') },
             { path: '/', component: Helper.loadComponent('configuration.js') },
         ]
